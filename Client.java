4aa9adcc81547be2fdde6e05bd88408540fe7292
Maria-Elena Dobrovolschi <marilenadobrovolschi10@gmail.com>
2020-07-13 11:26:10 +0300

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,10 +1,10 @@
-package com.company;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.*;
 
 public class Client {
     public static void main(String[] a) throws IOException, ClassNotFoundException {
@@ -12,18 +12,23 @@ public class Client {
         Socket socket;
         ObjectOutputStream output;
         ObjectInputStream input;
+        Scanner scan=new Scanner(System.in);
         socket = new Socket(host.getHostName(), 8080);
 
-        input = new ObjectInputStream(socket.getInputStream());
-        String message = (String) input.readObject();
-        System.out.println(message + "World");
+
 
         output = new ObjectOutputStream(socket.getOutputStream());
-        output.writeObject("World");
+        String name;
+        System.out.println("Enter your name!");
+        name=scan.next();
+        output.writeObject(name);
+        input = new ObjectInputStream(socket.getInputStream());
+        String message = (String) input.readObject();
+        System.out.println(name + message);
 
-        //input.close();
-        //output.close();
-        //socket.close();
+        input.close();
+        output.close();
+        socket.close();
     }
 
 
